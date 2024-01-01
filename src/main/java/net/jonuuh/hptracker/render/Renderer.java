7d3a0c62daa998700b7f6e3bc36311c2239b3391
jonuuh <jhammond.123x@gmail.com>
2024-01-01 03:55:01 -0800

    moved most of Utilities and Player to inner classes in Renderer

--- src/main/java/net/jonuuh/hptracker/render/Renderer.java
+++ src/main/java/net/jonuuh/hptracker/render/Renderer.java
@@ -1,20 +1,27 @@
 package net.jonuuh.hptracker.render;
 
 import net.jonuuh.hptracker.config.Config;
-import net.jonuuh.hptracker.util.Player;
 import net.jonuuh.hptracker.util.Utilities;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.WorldSettings;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Renderer
 {
     private final Minecraft mc;
     private final Config config;
+    private final RendererUtils rendererUtils;
 
     private String message;
     private int time;
@@ -26,6 +33,7 @@ public class Renderer
     {
         this.mc = mc;
         this.config = config;
+        this.rendererUtils = new RendererUtils();
     }
 
     /**
@@ -38,7 +46,7 @@ public class Renderer
     {
         if (mc.theWorld != null)
         {
-            Player player = Utilities.getTargetPlayer(mc, config.getTargetPlayerNames(), config.getMaxDistance());
+            RendererUtils.Player player = rendererUtils.getMinHealthValidTargetPlayer();
             if (player != null)
             {
                 if ((player.getHealth() / player.getMaxHealth()) * 100 <= config.getThresholdHPPercent())
@@ -107,6 +115,122 @@ public class Renderer
     private FontRenderer getFontRenderer()
     {
         return mc.fontRendererObj;
+    }
+
+    private class RendererUtils
+    {
+        /**
+         * Creates and gets a Player object, containing the name, max health, current health, and name color
+         *
+         * @return the player OR null if there no non-spectator target players within the max config distance
+         */
+        public Player getMinHealthValidTargetPlayer()
+        {
+            String minHealthTargetPlayerName = getMinHealthTargetPlayerName();
+            if (minHealthTargetPlayerName != null)
+            {
+                float maxHealth = getPlayerMaxHealthByName(minHealthTargetPlayerName);
+                float health = getPlayerHealthByName(minHealthTargetPlayerName);
+                int color = getPlayerColorHexByName(minHealthTargetPlayerName);
+                return new Player(minHealthTargetPlayerName, maxHealth, health, color);
+            }
+            return null;
+        }
+
+        /**
+         * Gets name of the valid target player with least health OR null if no valid target players.
+         */
+        private String getMinHealthTargetPlayerName()
+        {
+            Set<String> validPlayers = config.getTargetPlayerNames().stream().filter(this::verifyPlayer).collect(Collectors.toSet());
+            return (validPlayers.size() != 0) ? Collections.min(validPlayers, Comparator.comparing(this::getPlayerHealthByName)) : null;
+        }
+
+        /**
+         * Verify by name whether an EntityPlayer exists in WorldClient, is not a spectator, and is within max config distance<br>
+         * Note: WorldClient assumed != null
+         */
+        private boolean verifyPlayer(String name)
+        {
+            EntityPlayer player = mc.theWorld.getPlayerEntityByName(name);
+            return player != null && !isPlayerSpectatorByName(name) && mc.thePlayer.getDistanceToEntity(player) <= config.getMaxDistance();
+        }
+
+        /**
+         * Verify by name whether an EntityPlayer is a spectator.<br>
+         * Note: NetHandlerPlayClient and player NetworkPlayerInfo assumed != null
+         */
+        private boolean isPlayerSpectatorByName(String name)
+        {
+            return mc.getNetHandler().getPlayerInfo(name).getGameType() != WorldSettings.GameType.SPECTATOR;
+        }
+
+        /**
+         * Gets a EntityPlayer's max health by name.<br>
+         * Note: WorldClient and EntityPlayer assumed != null
+         */
+        private float getPlayerMaxHealthByName(String name)
+        {
+            return mc.theWorld.getPlayerEntityByName(name).getMaxHealth();
+        }
+
+        /**
+         * Gets a EntityPlayer's health by name.<br>
+         * Note: WorldClient and EntityPlayer assumed != null
+         */
+        private float getPlayerHealthByName(String name)
+        {
+            return mc.theWorld.getPlayerEntityByName(name).getHealth();
+        }
+
+        /**
+         * Gets an EntityPlayer's displayName color hex by name.<br>
+         * Note: WorldClient and EntityPlayer assumed != null
+         */
+        private int getPlayerColorHexByName(String name)
+        {
+            EntityPlayer player = mc.theWorld.getPlayerEntityByName(name);
+            String formattedName = player.getDisplayName().getFormattedText();
+            String unformattedName = player.getName();
+            char formattingColorCode = formattedName.charAt(formattedName.indexOf(unformattedName) - 1);
+            return Utilities.formattingColorCodeToHex.get(formattingColorCode);
+        }
+
+        private class Player
+        {
+            private final String name;
+            private final float maxHealth;
+            private final float health;
+            private final int color;
+
+            private Player(String name, float maxHealth, float health, int color)
+            {
+                this.name = name;
+                this.maxHealth = maxHealth;
+                this.health = health;
+                this.color = color;
+            }
+
+            private String getName()
+            {
+                return name;
+            }
+
+            private float getMaxHealth()
+            {
+                return maxHealth;
+            }
+
+            private float getHealth()
+            {
+                return health;
+            }
+
+            private int getColor()
+            {
+                return color;
+            }
+        }
     }
 }
 
--- src/main/java/net/jonuuh/hptracker/util/Utilities.java
+++ src/main/java/net/jonuuh/hptracker/util/Utilities.java
@@ -1,176 +1,42 @@
 package net.jonuuh.hptracker.util;
 
-import net.jonuuh.hptracker.config.TargetPlayerNameSet;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.world.WorldSettings;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * Misc. Utilities.
  */
 public class Utilities
 {
-    /**
-     * Creates and gets a Player object, containing the name, current health, and name color
-     *
-     * @param mc                  the minecraft object
-     * @param targetPlayerNameSet the target player names
-     * @param maxDist             the max valid distance away from the client player
-     * @return the player OR null if there no non-spectator target players within the max config distance
-     */
-    public static Player getTargetPlayer(Minecraft mc, TargetPlayerNameSet<String> targetPlayerNameSet, float maxDist)
-    {
-        String minHealthTargetPlayerName = getMinHealthTargetPlayerName(mc, targetPlayerNameSet, maxDist);
-        if (minHealthTargetPlayerName != null)
-        {
-            float maxHealth = getPlayerMaxHealthByName(mc, minHealthTargetPlayerName);
-            float health = getPlayerHealthByName(mc, minHealthTargetPlayerName);
-            int color = getPlayerColorHexByName(mc, minHealthTargetPlayerName);
-            return new Player(minHealthTargetPlayerName, maxHealth, health, color);
-        }
-        return null;
-    }
+    public static final Map<Character, Integer> formattingColorCodeToHex = initMap();
 
-    /**
-     * Gets name of target player with least health.
-     *
-     * @param mc                  the minecraft object
-     * @param targetPlayerNameSet the target player names
-     * @param maxDist             the max valid distance away from the client player
-     * @return the min health target player OR null if no valid target players exist
-     */
-    private static String getMinHealthTargetPlayerName(Minecraft mc, TargetPlayerNameSet<String> targetPlayerNameSet, float maxDist)
+    private static Map<Character, Integer> initMap()
     {
-        Set<String> validPlayers = targetPlayerNameSet.stream().filter(player -> verifyPlayer(mc, player, maxDist)).collect(Collectors.toSet());
-        return (validPlayers.size() != 0) ? Collections.min(validPlayers, Comparator.comparing(player -> getPlayerHealthByName(mc, player))) : null;
-    }
-
-    /**
-     * Verify a player by name.<br>
-     * Note: WorldClient assumed != null
-     *
-     * @param mc      the minecraft object
-     * @param name    the name
-     * @param maxDist the max valid distance away from the client player
-     * @return whether an EntityPlayer with the name exists in WorldClient, is not a spectator, and is within the max config distance
-     */
-    private static boolean verifyPlayer(Minecraft mc, String name, float maxDist)
-    {
-        EntityPlayer player = mc.theWorld.getPlayerEntityByName(name);
-        return player != null && !isPlayerSpectatorByName(mc, name) && mc.thePlayer.getDistanceToEntity(player) <= maxDist;
-    }
-
-    /**
-     * Is the player a spectator.<br>
-     * Note: NetHandlerPlayClient and player NetworkPlayerInfo assumed != null
-     *
-     * @param mc   the minecraft object
-     * @param name the name
-     * @return whether the player with the name is not a spectator.
-     */
-    private static boolean isPlayerSpectatorByName(Minecraft mc, String name)
-    {
-        return mc.getNetHandler().getPlayerInfo(name).getGameType() != WorldSettings.GameType.SPECTATOR;
-    }
-
-    /**
-     * Gets a EntityPlayer's max health by name.<br>
-     * Note: WorldClient and EntityPlayer assumed != null
-     *
-     * @param mc   the minecraft object
-     * @param name the name
-     * @return the EntityPlayer's health
-     */
-    private static float getPlayerMaxHealthByName(Minecraft mc, String name)
-    {
-        return mc.theWorld.getPlayerEntityByName(name).getMaxHealth();
-    }
-
-    /**
-     * Gets a EntityPlayer's health by name.<br>
-     * Note: WorldClient and EntityPlayer assumed != null
-     *
-     * @param mc   the minecraft object
-     * @param name the name
-     * @return the EntityPlayer's health
-     */
-    private static float getPlayerHealthByName(Minecraft mc, String name)
-    {
-        return mc.theWorld.getPlayerEntityByName(name).getHealth();
-    }
-
-    /**
-     * Gets an EntityPlayer's displayName color hex by name.<br>
-     * Note: WorldClient and EntityPlayer assumed != null
-     *
-     * @param mc   the minecraft object
-     * @param name the name
-     * @return the EntityPlayer's displayName color hex
-     */
-    private static int getPlayerColorHexByName(Minecraft mc, String name)
-    {
-        EntityPlayer player = mc.theWorld.getPlayerEntityByName(name);
-        String formattedName = player.getDisplayName().getFormattedText();
-        String unformattedName = player.getName();
-        char formattingColorCode = formattedName.charAt(formattedName.indexOf(unformattedName) - 1);
-        return formattingColorCodeToHex(formattingColorCode);
-    }
-
-    /**
-     * Convert a minecraft formatting color code to a color hex value.
-     *
-     * @param code the minecraft formatting color code
-     * @return a color hex value
-     */
-    private static int formattingColorCodeToHex(char code)
-    {
-        switch (code)
-        {
-            case '0':
-                return 0x000000;
-            case '1':
-                return 0x0000AA;
-            case '2':
-                return 0x00AA00;
-            case '3':
-                return 0x00AAAA;
-            case '4':
-                return 0xAA0000;
-            case '5':
-                return 0xAA00AA;
-            case '6':
-                return 0xFFAA00;
-            case '7':
-                return 0xAAAAAA;
-            case '8':
-                return 0x555555;
-            case '9':
-                return 0x5555FF;
-            case 'a':
-                return 0x55FF55;
-            case 'b':
-                return 0x55FFFF;
-            case 'c':
-                return 0xFF5555;
-            case 'd':
-                return 0xFF55FF;
-            case 'e':
-                return 0xFFFF55;
-            case 'f':
-                return 0xFFFFFF;
-            default:
-                return 0xFFFFFF; //
-        }
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('0', 0x000000);
+        map.put('1', 0x0000AA);
+        map.put('2', 0x00AA00);
+        map.put('3', 0x00AAAA);
+        map.put('4', 0xAA0000);
+        map.put('5', 0xAA00AA);
+        map.put('6', 0xFFAA00);
+        map.put('7', 0xAAAAAA);
+        map.put('8', 0x555555);
+        map.put('9', 0x5555FF);
+        map.put('a', 0x55FF55);
+        map.put('b', 0x55FFFF);
+        map.put('c', 0xFF5555);
+        map.put('d', 0xFF55FF);
+        map.put('e', 0xFFFF55);
+        map.put('f', 0xFFFFFF);
+        return map;
     }
 
     /**
